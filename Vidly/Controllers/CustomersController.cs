9877142d2e8e2d909ad6cb4f35095df1bc8fce92
Justin Horein <justinhorein@gmail.com>
2023-05-05 21:57:11 -0700

    fin basic customer + movies views and controllers

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+ 
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = GetCustomers();
+
+            return View(customers);
+        }
+
+        [Route("Customers/Details/{id}")]
+        public ActionResult Details(int Id)
+        {
+            var customers = GetCustomers();
+
+            foreach (Customer custy in customers) 
+            {
+                if (custy.Id == Id)
+                    return View(custy);
+            }
+
+            return HttpNotFound();
+        }
+
+        private List<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+                new Customer { Name = "John Smith", Id = 1 },
+                new Customer { Name = "Mary Williams", Id = 2 }
+            };
+        }
+    }
+}
--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -4,16 +4,33 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
         // GET: Movies
-        public ActionResult Random()
+        public ActionResult Index()
         {
-            var movie = new Movie() { Name = "Shrek" };
-            return View(movie);
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        private List<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Mulan" }
+            };
+        }
+
+        [Route("movies/released/{year}/{month:regex(\\d{4}):range(1, 12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
         }
     }
 }
